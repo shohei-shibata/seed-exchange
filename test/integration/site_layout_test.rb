36ac8fbf6d3045858f7b1a43243484b264d9d7e8
Shohei Shibata <shohei.shibata.dev@gmail.com>
2020-05-09 17:31:18 -0400

    sign up route and placeholder page created

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,12 +1,22 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  test "layout links" do
+  test "nav links" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+  end
+
+  test "contact page" do
+    get contact_path
+    assert_select "title", full_title("Contact")
+  end
+
+  test "sign up page" do
+    get signup_path
+    assert_select "title", full_title("Sign Up")
   end
 end
