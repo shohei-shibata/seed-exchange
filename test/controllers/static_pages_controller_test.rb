1a491a8b4f05e0d3e958e8fd549b87a3fd18b38f
Shohei Shibata <shohei.shibata.dev@gmail.com>
2020-05-09 06:57:11 -0400

    integration test to check nav links, add contact page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,23 +5,29 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get root_url
     assert_response :success
   end
-
+  
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Seed Exchange"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | Seed Exchange"
   end
 
   test "should get about" do 
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | Seed Exchange"
+  end
+
+  test "should get contact" do 
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | Seed Exchange"
   end
 
 end
