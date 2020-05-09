779937e7a546ea5d7d1af9b5bce22b6fe3e4446f
Shohei Shibata <shohei.shibata.dev@gmail.com>
2020-05-08 20:54:25 -0400

    refactor page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Returns the full page title on a per-page basis
+  def full_title(page_title='')
+    base_title = "Seed Exchange"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,7 +9,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Seed Exchange"
+    assert_select "title", "Seed Exchange"
   end
 
   test "should get help" do
