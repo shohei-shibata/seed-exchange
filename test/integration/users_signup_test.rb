d77b78a8573a016fabd80e8c9f4f39080440d1d8
Shohei Shibata <shohei.shibata.dev@gmail.com>
2020-05-11 16:09:33 -0400

    login/logout

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -18,6 +18,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'                               
+    assert is_logged_in?
     assert_select 'div.alert-success'
   end
   test "invalid signup information" do
