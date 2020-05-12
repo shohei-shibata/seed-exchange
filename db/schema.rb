2381cdc578a1b13750d746fa18e0b796924c3ff7
Shohei Shibata <shohei.shibata.dev@gmail.com>
2020-05-12 05:34:42 -0400

    remember me box added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_093150) do
+ActiveRecord::Schema.define(version: 2020_05_12_084341) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_093150) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
