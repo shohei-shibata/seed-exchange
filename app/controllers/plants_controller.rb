8ee82b7499b1e8ba651ee3a98b8f313fc021626d
Shohei Shibata <shohei.shibata.dev@gmail.com>
2020-05-21 07:14:20 -0400

    create actions to create and destroy plants

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,9 @@
 class PlantsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+
+  def create
+  end
+
+  def destroy
+  end
 end
--- test/controllers/plants_controller_test.rb
+++ test/controllers/plants_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class PlantsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @plant = plants(:one)
+  end
+
+  test "should redirect create when not logged in" do
+    assert_no_difference 'Plant.count' do
+      post plants_path, params: { plant: { name: "Lorem ipsum",
+                                           name_latin: "Lorem ipsum" } }
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'Plant.count' do
+      delete plant_path(@plant)
+    end
+    assert_redirected_to login_url
+  end
 end
